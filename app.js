63f4b75504ae9f5237e9b07d3ead83ccbbc24b09
Andres Felipe Rincon Lopez <Andres Felipe Rincon Lopez>
2017-04-23 18:27:38 -0500

    mensaje

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('#seccion1').empty();
     for (var i=0; i< agregar; i++)
     {
-      $("#seccion1").append("<div class='form-group row'> <label for='example-text-input' class='col-2 col-form-label'><b>Nombre de la mascota</b></label><div class='col-10'><input class='form-control' name='nombredemascota1-collar1' type='text' id='example-text-input' maxlength='20'/></div><label class='aparte'>Tamaño de la mascota:</label><input class='campo' name='tamaño1' type='radio' value='Pequeño'/> Pequeño <br ><input class='campo' name='tamaño1' type='radio' value='Mediano'/> Mediano<br ><input class='campo' name='tamaño1' type='radio' value='Grande'/> Grande  <br ></div>")
+      $("#seccion1").append("<div class='form-group row'> <label for='example-text-input' class='col-2 col-form-label'><b>Nombre de la mascota</b></label><div class='col-10'><input class='form-control' name='nombredemascota1-collar1"+i+"' type='text' id='example-text-input' maxlength='20'/></div><label class='aparte'>Tamaño de la mascota:</label><input class='campo' name='tamaño1"+i+"' type='radio' value='Pequeño'/> Pequeño <br ><input class='campo' name='tamaño1"+i+"' type='radio' value='Mediano'/> Mediano<br ><input class='campo' name='tamaño1"+i+"' type='radio' value='Grande'/> Grande  <br ></div>")
     }
 
   });
@@ -25,7 +25,7 @@ $(document).ready(function() {
       $('#seccion2').empty();
       for (var i=0; i< agregar2; i++)
       {
-        $("#seccion2").append("<div class='form-group row'><label>Nombre de la mascota</label><div class= 'col-10'><input class='form-control' type='text' id='example-text-input' maxlength='20'></div><br ><label class='aparte'>Tamaño de la mascota:</label><input class='campo' name='2tamaño1' type='radio' value='Pequeño'/> Pequeño <br ><input class='campo' name='2tamaño1' type='radio' value='Mediano'/> Mediano<br ><input class='campo' name='2tamaño1' type='radio' value='Grande'/> Grande<br ><br ><div>")
+        $("#seccion2").append("<div class='form-group row'><label>Nombre de la mascota</label><div class= 'col-10'><input class='form-control' type='text' id='example-text-input' name='nombredemascota2-collar2"+i+"' maxlength='20'></div><br ><label class='aparte'>Tamaño de la mascota:</label><input class='campo' name='2tamaño1"+i+"' type='radio' value='Pequeño'/> Pequeño <br ><input class='campo' name='2tamaño1"+i+"' type='radio' value='Mediano'/> Mediano<br ><input class='campo' name='2tamaño1' type='radio' value='Grande'/> Grande<br ><br ><div>")
       }
 
     });
@@ -66,5 +66,40 @@ $(document).ready(function() {
           });
           */
 
+          $.ajaxSetup({
+                  contentType: "application/json; charset=utf-8",
+                  dataType: "json"
+              });
+              /**
+                Tomado de http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+              */
+              function objectifyForm(formArray) {
+                var returnArray = {};
+                for (var i = 0; i < formArray.length; i++)
+                {
+                  returnArray[formArray[i]['name']] = formArray[i]['value'];
+                }
+                return returnArray;
+        }
+
+              $(document).ready(function() {
+                  $('form').submit(function(e) {
+                      var send = JSON.stringify(objectifyForm($(this).serializeArray()));
+                      console.log(send);
+                      $.ajax({
+                          url: "http://couchdb.contraslash.com/animalssaving/",
+                          type: "POST",
+                          data: send,
+                          success: function (sreg, status, jqXHR) {
+                              alert(JSON.stringify(sreg));
+                          },
+                          error: function (jqXHR, status) {
+                              alert(JSON.stringify(jqXHR));
+                          }
+                      });
+                      return false;
+                  });
+              });
 
 });
+//http://couchdb.contraslash.com/animalssaving/_all_docs
